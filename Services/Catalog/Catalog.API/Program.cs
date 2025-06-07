a52db9772e1c4ea486756a751f4fe498e7822aa1
dimash.abdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-06-07 13:35:57 +0500

    01 First Microservice Catalog.API with Vertical Slice Architecture and CQRS

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => { cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly); });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
+
 
 app.Run();
